96af87d9401151660486ac41a7919d61fa98dc90
Mikheil Berishvili <mikheil.berishvili12@gmail.com>
2026-08-19 15:20:27 +0400

    Drop build number from displayed version

--- Readly/Settings/Sections/UpdatesSettingsView.swift
+++ Readly/Settings/Sections/UpdatesSettingsView.swift
@@ -79,8 +79,6 @@ struct UpdatesSettingsView: View {
   }
 
   static var version: String {
-    let short = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "—"
-    let build = Bundle.main.infoDictionary?["CFBundleVersion"] as? String ?? "—"
-    return "\(short) (\(build))"
+    Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String ?? "—"
   }
 }
